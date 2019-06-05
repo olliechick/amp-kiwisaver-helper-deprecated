82e91154dcab2901803da633b40c9c9c94ce3f6e
Ollie Chick <och26@uclive.ac.nz>
2019-06-06 00:29:26 +1200

    Use text entered into textbox instead of input.txt
    This was done by:
    - Creating a class ProcessReport
    - Move GUI functions into it
    - Every time textbox is edited, update csv_contents and left_to_get,
      which are now class variables
    - Use those variables when clicking buttons

--- process_report.py
+++ process_report.py
@@ -30,11 +30,10 @@ def read_file(filename):
     return file.read()
 
 
-def process_data(input_filename):
+def process_data(file_contents):
     """Returns:
      - the contents of a CSV file of contributions that count
      - the amount left until you have given MAXIMUM_NECESSARY_PAYMENT"""
-    file_contents = read_file(input_filename)
     items = ''.join(file_contents.split("Effective date Description Account Amount Units")[1:]).split()
     accounts = read_file('accounts.txt').split('\n')
 
@@ -60,40 +59,61 @@ def process_data(input_filename):
     return output_csv, left_to_give
 
 
-def save_csv():
-    output_csv, total = process_data('input.txt')
-    write_to_file('output.csv', output_csv)
+class ProcessReport:
+    csv_contents = ''
+    left_to_get = MAXIMUM_NECESSARY_PAYMENT
 
+    def save_csv(self):
+        write_to_file('output.csv', self.csv_contents)
 
-def display_left_to_get():
-    output_csv, total = process_data('input.txt')
-    messagebox.showinfo("Left to get", '$' + str(total.__round__(2)))
+    def display_left_to_get(self):
+        messagebox.showinfo("Left to get", '$' + str(self.left_to_get.__round__(2)))
 
+    def launch_gui(self):
+        root = tkinter.Tk()
+        root.title("AMP KiwiSaver helper")
+        big_frame = ttk.Frame(root)
+        big_frame.pack(fill='both', expand=True)
 
-def launch_gui():
-    root = tkinter.Tk()
-    root.title("AMP KiwiSaver helper")
-    big_frame = ttk.Frame(root)
-    big_frame.pack(fill='both', expand=True)
+        label = ttk.Label(big_frame, text="Copy and paste the contents of the report PDF below:")
+        label.pack()
 
-    label = ttk.Label(big_frame, text="Copy and paste the contents of the report PDF below:")
-    label.pack()
+        # Text box
 
-    entry = tkinter.Text(big_frame)
-    entry.pack(expand=1, fill=tkinter.BOTH)
+        def textbox_updated(*args):
+            # check the text
+            textbox_contents = textbox.get(1.0, tkinter.END)
+            self.csv_contents, self.left_to_get = process_data(textbox_contents)
 
-    show_left_to_get_button = ttk.Button(big_frame, text="Show left to get", command=display_left_to_get)
-    save_button = ttk.Button(big_frame, text="Save CSV", command=save_csv)
+        text_frame = ttk.Frame(big_frame)
+        text_frame.pack(fill='both', expand=True)
 
-    show_left_to_get_button.pack(in_=big_frame, side='left', expand=1, fill=tkinter.X)
-    save_button.pack(in_=big_frame, side='left', expand=1, fill=tkinter.X)
+        report_text = tkinter.StringVar()
+        textbox = tkinter.Text(text_frame)
+        scrollbar = ttk.Scrollbar(text_frame)
 
-    root.geometry('300x500')
-    root.mainloop()
+        scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+        textbox.pack(expand=1, fill=tkinter.BOTH)
+        scrollbar.config(command=textbox.yview)
+        textbox.config(yscrollcommand=scrollbar.set)
+        textbox.bind('<KeyRelease>', textbox_updated)
+
+        # Buttons
+
+        show_left_to_get_button = ttk.Button(big_frame, text="Show left to get", command=self.display_left_to_get)
+        save_button = ttk.Button(big_frame, text="Save CSV", command=self.save_csv)
+
+        show_left_to_get_button.pack(in_=big_frame, side=tkinter.LEFT, expand=1, fill=tkinter.X)
+        save_button.pack(in_=big_frame, side=tkinter.LEFT, expand=1, fill=tkinter.X)
+
+        # Set up
+
+        root.geometry('300x500')
+        root.mainloop()
 
 
 def main():
-    launch_gui()
+    ProcessReport().launch_gui()
 
 
 if __name__ == '__main__':
